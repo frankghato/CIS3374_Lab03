a940aaf1c122aae30797ff8797cf1e2dba80d0e9
frankghato <59925619+frankghato@users.noreply.github.com>
2021-09-13 20:45:41 -0400

    added assets and a new shiny button

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function changeText()
 {
     document.getElementById("text").innerHTML = "You clicked the button. Nice.";
 }
+
+function changeImage()
+{
+    document.getElementById("img").src = "assets/download.png"; 
+    document.getElementById("text").innerHTML = "how could you";
+}
